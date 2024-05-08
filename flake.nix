3c65f9c159e56e71ca2855d8a9337628a40846ac
KhemIngkapat <60930364+KhemIngkapat@users.noreply.github.com>
2024-05-08 09:37:03 +0700

    create nix flake for portability

---
+++ flake.nix
@@ -0,0 +1,22 @@
+{
+  description = "benchmarking 5 sorting algos";
+
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem
+      (system:
+        let pkgs = nixpkgs.legacyPackages.${system}; in
+        {
+          devShells.default = pkgs.mkShell {
+                nativeBuildInputs = with pkgs; [
+                python311
+                python311Packages.pandas
+                python311Packages.numpy
+                python311Packages.matplotlib
+                gcc
+                ];
+            };
+        }
+      );
+}
