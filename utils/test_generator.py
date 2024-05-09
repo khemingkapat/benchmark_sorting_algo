595a3d8b5f228d26a3a0aebf6b958b8908ededc9
KhemIngkapat <60930364+KhemIngkapat@users.noreply.github.com>
2024-05-09 18:48:09 +0700

    outlining the test generator file

--- utils/test_generator.py
+++ utils/test_generator.py
@@ -1,10 +1,23 @@
 from abc import ABC,abstractmethod
 
-class Test_Generator(ABC):
+class Writer:
+    def __init__(self):
+        pass
 
-    @abstractmethod
-    def get_input(self):
-        pass
-    
-    def write_output(self):
-        pass
+    def write_output(self,filename,*args,arr):
+        file = open(filename)
+        
+        list_str = lambda x : "\n".join(list(map(str,x)))
+
+        arg = list_str(args)
+        lines = list_str(arr)
+        file.writelines(arg)
+        file.writelines("START\n")
+        file.writelines(lines)
+
+        file.close()
+
+
+
+
+
