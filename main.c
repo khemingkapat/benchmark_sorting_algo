d33769e05d5d227502896a54508df7d1d0033a1b
KhemIngkapat <60930364+KhemIngkapat@users.noreply.github.com>
2024-05-09 09:36:13 +0700

    read to array, eof error

--- main.c
+++ main.c
@@ -12,14 +12,41 @@ int main() {
 
     char buffer[10];
 
-    do{
-        fgets(buffer,10,file); // reading buffer
-        buffer[strcspn(buffer,"\n")] = 0; // strip the eol
-    }while(strcmp(buffer,"START"));
+    do {
+        fgets(buffer, 10, file);           // reading buffer
+        buffer[strcspn(buffer, "\n")] = 0; // strip the eol
+    } while (strcmp(buffer, "START"));
 
-    
+    int size = 0;
+    int *arr = (int *)malloc(sizeof(int) * size);
+    int idx = 0;
 
+    /* while (!feof(file)) { */
+    /*     size++; */
+    /*     arr = (int *)realloc(arr, sizeof(int) * size); */
+    /*     fgets(buffer, 10, file); */
+    /*     buffer[strcspn(buffer, "\n")] = 0; // strip the eol */
+    /*     printf("%d",atoi(buffer)); */
+    /*     arr[idx] = atoi(buffer); */
+    /*     idx++; */
+    /*  */
+    /*     fgets(buffer, 10, file); */
+    /*     buffer[strcspn(buffer, "\n")] = 0; // strip the eol */
+    /*     printf("%d",atoi(buffer)); */
+    /*      */
+    /* } */
 
+    do {
+        fgets(buffer, 10, file);
+        buffer[strcspn(buffer, "\n")] = 0; // strip the eol
+        printf("%d ",atoi(buffer));
 
+    }while(!feof(file));
+    fclose(file);
+
+    /* for(int i = 0;i<size;i++){ */
+    /*     printf("%d ",arr[i]); */
+    /* } */
+    puts("");
     return 0;
 }
