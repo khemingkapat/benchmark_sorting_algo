9cfa16f7953aae173d35827210d2585dbbe6028f
KhemIngkapat <60930364+KhemIngkapat@users.noreply.github.com>
2024-05-10 22:38:12 +0700

    adding time function

---
+++ utils/timereport.h
@@ -0,0 +1,22 @@
+#include "stdio.h"
+#include "stdlib.h"
+#include "time.h"
+
+#ifndef TIMEREPORT_H
+#define TIMEPEPORT_H
+
+double time_function(void (*sort)(int*,int),int *arr,int n){
+    clock_t begin = clock();
+    
+    sort(arr,n);
+
+    clock_t end = clock();
+
+    return (double)(end-begin)/CLOCKS_PER_SEC;
+}
+
+
+
+
+
+#endif 
