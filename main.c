7fa0b9beb6cad00a37a0cb1b493417fd1a2f4128
KhemIngkapat <60930364+KhemIngkapat@users.noreply.github.com>
2024-05-18 14:43:56 +0700

    adding puts and the it all suddenly work!?!

--- main.c
+++ main.c
@@ -1,8 +1,8 @@
 #include "stdio.h"
 #include "stdlib.h"
-#include <time.h>
 #include "utils/readfile.h"
 #include "utils/timereport.h"
+#include <time.h>
 
 void bubble_sort(int *arr, int n);
 
@@ -11,27 +11,21 @@ int main() {
     int f;
     int *arr = (int *)malloc(sizeof(int));
     int n = array_from_file(arr, "tests/normal1.txt");
-    time_function(bubble_sort, arr,n);
+    double time_used = time_function(bubble_sort, arr, n);
+    printf("time used = %lf ms\n",time_used*1000);
+
     return 0;
 }
 
-void swap(int *x,int *y){
-    int temp = *x;
-    *x = *y;
-    *y = temp;
-}
-
-
 void bubble_sort(int *arr, int n) {
+    int temp;
     for (int i = n; i >= 1; i--) {
         for (int j = 1; j < i; j++) {
-            /* printf("bubble at %d and %d\n",i,j); */
             if (arr[j - 1] > arr[j]) {
-                swap(&arr[j-1],&arr[j]);
+                temp = arr[j - 1];
+                arr[j - 1] = arr[j];
+                arr[j] = temp;
             }
         }
     }
 }
-
-
-
--- utils/timereport.h
+++ utils/timereport.h
@@ -5,15 +5,17 @@
 #ifndef TIMEREPORT_H
 #define TIMEPEPORT_H
 
-float time_function(void (*sort)(int*,int),int *arr,int n){
+double time_function(void (*sort)(int*,int),int *arr,int n){
     clock_t t = clock();
+    puts("about to sort");
     
     sort(arr,n);
+    puts("sorted");
 
     t = clock() - t;
 
-    float time_used = (float)t / CLOCKS_PER_SEC;
-    printf("tick used = %f\n",time_used);
+    double time_used = (double)t / CLOCKS_PER_SEC;
+    /* printf("tick used = %f\n",time_used); */
     return time_used;
 
 }
